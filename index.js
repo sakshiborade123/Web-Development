51a7368a9f07e38a575f7f973cc9c3654a2282d0
Sakshi Borade <boradesakshi494@gmail.com>
2026-01-07 21:14:58 +0530

    JavaScript

---
+++ index.js
@@ -0,0 +1,37 @@
+// console.log("Hello JavaScript")
+
+// var a= 4355;
+// console.log(a)
+
+// var b="Hello Javascript"
+// console.log(b)
+
+
+
+
+// document.writeln(b)
+// document.writeln("<br>")
+// document.writeln(a)
+// document.writeln("<br>")
+
+//------------------------------------------------------------------------
+// var a;//declaration
+// a = 10;//intilization
+// a =20;//updation
+
+// var a =30;
+// console.log(30)
+// document.writeln(a)
+
+//-----------------------------------------------------------------------------
+
+// // let b; //declaration
+// // b=20; // intilization
+// // b=30;  //updation
+// let b=50;
+// document.writeln(b)
+
+//------------------------------------------------------------------------------
+const a=10;
+console.log(a)
+document.writeln(a)
