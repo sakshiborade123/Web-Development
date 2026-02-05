80281ba0fd6bc1fc44e24fe62b3e1876ce9d2c49
Sakshi Borade <boradesakshi494@gmail.com>
2026-02-05 16:33:39 +0530

    Carts

--- Cart.js
+++ Cart.js
@@ -1,3 +1,24 @@
-let Watch=()=>{
-    console.log("Watch");
-}
+// Select all product divs
+let products = document.querySelectorAll("section div");
+
+products.forEach((product) => {
+    let addBtn = product.querySelector(".add");
+    let removeBtn = product.querySelector(".remove");
+    let countSpan = product.querySelector(".count");
+
+    let count = 0;
+
+    // Add to cart
+    addBtn.addEventListener("click", () => {
+        count++;
+        countSpan.innerText = count;
+    });
+
+    // Remove from cart
+    removeBtn.addEventListener("click", () => {
+        if (count > 0) {
+            count--;
+            countSpan.innerText = count;
+        }
+    });
+});
